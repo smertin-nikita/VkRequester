2e2b30dbeeff2e2e488e830ff1d1257947c6069f
Smertin Nikita Job <smertin-nikita@mail.ru>
2020-12-08 13:54:32 +0500

    Get mutual friends

--- VkRequester.py
+++ VkRequester.py
@@ -14,6 +14,7 @@ class VkUser:
             'access_token': self.__token,
             'v': self.version
         }
+
         info = requests.get(
             url=self.url+'users.get',
             params={
@@ -25,10 +26,24 @@ class VkUser:
         self.screen_name = info['response'][0]['screen_name']
 
     def __str__(self):
-        id = self.screen_name or self.id
+        id = self.screen_name or 'id' + self.id
         return f'https://vk.com/{id}'
+
+    def get_mutual(self, target_uid, source_uid=None):
+        source_uid = source_uid or self.id
+        friends_url = self.url + 'friends.getMutual'
+        friends_params = {
+            'source_uid': source_uid,
+            'target_uid': target_uid
+        }
+        res = requests.get(
+            url=friends_url,
+            params={**self.params, **friends_params}
+        )
+        return res.json()
 
 
 if __name__ == '__main__':
     vk_user = VkUser('10b2e6b1a90a01875cfaa0d2dd307b7a73a15ceb1acf0c0f2a9e9c586f3b597815652e5c28ed8a1baf13c', '5.126')
-    print(vk_user)
+    friends = vk_user.get_mutual('40187990', '72775614')
+    print(friends)
