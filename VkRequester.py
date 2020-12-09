b68bfcdc1ab73e94d914d70591d78cf9c4b3d322
Smertin Nikita Job <smertin-nikita@mail.ru>
2020-12-09 14:10:21 +0500

    Get token from file during init

--- VkRequester.py
+++ VkRequester.py
@@ -5,43 +5,44 @@ import requests
 from pprint import pprint
 
 
-def get_token(path):
-    try:
-        with open(path, 'r') as f:
-            token = f.readline()
-    except FileNotFoundError:
-        print("File not found")
-    return token
-
-
-class MyProfile:
+class VkUser:
     """Represents entity vk user"""
 
     url = 'https://api.vk.com/method/'
 
-    def __init__(self, token, version):
-        self.__token = token
+    # todo:  Если токен отсутвует в конфиг файле или истек - сделать запрос на получения токена
+    def __init__(self, version='5.126', uid=None):
+        self.__token = self.__get_token()
         self.version = version
         self.params = {
             'access_token': self.__token,
             'v': self.version
         }
 
+        # todo: Пока жестко закодированы. Либо расшрить поля,
+        #  либо добавлять списком или словарем как параметр или иначе...
+        fields = ['screen_name']
+
         response = requests.get(
             url=self.url+'users.get',
             params={
                 **self.params,
-                'fields': 'screen_name'
+                'user_ids': uid,
+                'fields': fields
             }
         )
+        # todo: Почему-то не отлвливает ошибки VkApi
         response.raise_for_status()
-        info = response.json()
-        self.id = info['response'][0]['id']
-        self.screen_name = info['response'][0]['screen_name']
+
+        print(response.json())
+        info = response.json()['response'][0]
+
+        self.id = info['id']
+        self.screen_name = info['screen_name']
 
     def __str__(self):
-        id = self.screen_name or 'id' + self.id
-        return f'https://vk.com/{id}'
+        uid = self.screen_name or 'id' + self.id
+        return f'https://vk.com/{uid}'
 
     def __and__(self, target_uid):
         friends_url = self.url + 'friends.getMutual'
@@ -49,17 +50,32 @@ class MyProfile:
             'source_uid': self.id,
             'target_uid': target_uid
         }
-        res = requests.get(
+
+        response = requests.get(
             url=friends_url,
             params={**self.params, **friends_params}
         )
-        return res.json()
+        response.raise_for_status()
+        return response.json()
+
+    TOKEN_PATH = 'keys/token'
+
+    def __get_token(self):
+        path = op.join(os.getcwd(), self.TOKEN_PATH)
+        try:
+            with open(path, 'r') as f:
+                token = f.readline()
+        except FileNotFoundError:
+            print("File not found")
+        return token
 
 
 if __name__ == '__main__':
-    token_path = get_token(op.join(os.getcwd(), 'keys/token'))
-    my_profile = MyProfile(token_path, '5.126')
-    friends = my_profile.get_mutual('40187990')
-    print(friends)
+    my_profile = VkUser()
+    print(my_profile)
+    another_user = VkUser(uid='40187990')
+    print(another_user)
+    # friends = my_profile.get_mutual('40187990')
+    # print(friends)
 
 
